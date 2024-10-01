41237d613cc52da017a69e758712056067d5ed63
Danil019 <100717092+Danil019@users.noreply.github.com>
2024-10-01 21:44:14 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(first, second):
+    if second > 0:
+        result_div = first / second
+        return result_div
+    else:
+        return "На ноль делить нельзя"
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+
+def divide(first, second):
+    if second > 0:
+        result_div = first / second
+        return result_div
+    else:
+        return inf
